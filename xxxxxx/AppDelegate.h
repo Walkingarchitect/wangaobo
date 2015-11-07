84a38803546e64db688b9b6ed90f85b08c0b9ada
王奥博 <634676589@qq.com>
2015-11-07 23:15:02 +0800

    first
    first

---
+++ xxxxxx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xxxxxx
+//
+//  Created by 王奥博 on 15/11/7.
+//  Copyright © 2015年 王奥博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xxxxxx/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xxxxxx
+//
+//  Created by 王奥博 on 15/11/7.
+//  Copyright © 2015年 王奥博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
